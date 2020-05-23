1098ef5ef20aa6a3c1b6a76a580385b88f45fe48
AashiqinCode <aashiqahmed10101@ggmail.com>
2020-05-23 13:51:29 +0530

    playing with set state

--- src/App.js
+++ src/App.js
@@ -9,16 +9,26 @@ class App extends Component {
     persons: [
       {
         name: "AashiqAhmed",
-        age: 12,
+      },
+      {
+        name: "Maxy",
+      },
+      {
+        name: "",
       },
     ],
   };
-  switchEventHandler = () => {
+  switchEventHandler = (newName) => {
     console.log("I clicked!");
-    
+
     // DONT DO THIS=>this.state.persons[0].name='Allah';
+
     this.setState({
-      persons: [{ name: "Aadhil Ahmed" }],
+      persons: [
+        { name: "Aadhil Ahmed" },
+        { name: newName },
+        { name: "Abdulah" },
+      ],
     });
   };
 
@@ -29,13 +39,24 @@ class App extends Component {
         <p>Age:21</p> */}
 
         <button onClick={this.switchEventHandler}>Switch</button>
+
         <PersonComponent name="Aashiq">
           This is a children content
         </PersonComponent>
 
-        <PersonComponent name={this.state.persons[0].name} />
+        <PersonComponent
+          name={this.state.persons[0].name}
+          click={this.switchEventHandler}
+        />
 
-        <PersonComponent />
+        {/* TO change the value of state using bind */}
+
+        <PersonComponent
+          click={this.switchEventHandler.bind(this, "Zubair Ahmed")}
+          name={this.state.persons[2].name}
+        >
+          Click above me to see the change
+        </PersonComponent>
       </div>
       // <h1>Hi</h1> => We cannot use like this,as all the tags should be wrapped inside one root element
       // React.createElement('div',{className:'p1'},React.createElement('h1',null,'This is using createElement()'))
--- src/Person/persons.js
+++ src/Person/persons.js
@@ -3,15 +3,17 @@ import React, { Component } from "react";
 class Persons extends Component {
   render() {
     return (
-    <div>
-      <p>
-        This is person {this.props.name} component {Math.floor(Math.random() * 30)}
-      
-      </p>
-      
-    <p>{this.props.children}</p>
-        </div>
-     
+      <div>
+        <p onClick={this.props.click}>
+          This is person {this.props.name} with a random number
+          {Math.floor(Math.random() * 30)}
+          {/* <input type="text" onChange={this.props.changed} /> */}
+        </p>
+
+        <p>{this.props.children}</p>
+      </div>
+
+      //  Props are immutable and state is mutable
       // For functions ,we can use props as a args and use.
       // For class like this,we want to use 'this.props.<attr>'
     );
