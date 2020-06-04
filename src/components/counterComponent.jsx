bd32a126e456f42338d63c66629ec0d6b4e7ed01
AashiqinCode <aashiqahmed10101@ggmail.com>
2020-06-04 09:37:26 +0530

    Refractored to Single source of truth and added reset button

--- src/components/counterComponent.jsx
+++ src/components/counterComponent.jsx
@@ -5,39 +5,20 @@
 //used the shortcut from 'Simple React Snippet'=> cc
 import React, { Component } from "react"; //used the shortcut from 'Simple React Snippet' =>imrc
 class Counter extends Component {
-  state = {
-    count: 0,
-    tags: ["tag1"],
-  };
+  // A state is local or private to the component
 
-  
-  constructor() {
-    super(); //Using super() to get the class properties
+  //! We are getting the control of the parent component so no need of state in this component,"Single Source of Truth"
 
-    // console.log("This is a constructor",this);
+  // state = {
+  //   count: this.props.counter.value,
+  //   tags: ["tag1"],
+  // };
 
-    // this.handleIncrement = this.handleIncrement.bind(this);
-  }
   //Used to use styles as JS  object in HTML tags
   styles = {
     fontWeight: "bold",
     fontSize: 17,
   };
-
-  // Rendering tags based on Condition
-  renderTags() {
-    if (this.state.tags.length === 0) {
-      return <p>There are no tags!</p>;
-    } else {
-      return (
-        <li>
-          {this.state.tags.map((tag) => (
-            <li>{tag}</li>
-          ))}
-        </li>
-      );
-    }
-  }
 
   // Handling Event function using array function which inherits 'this' normally
 
@@ -45,37 +26,30 @@ class Counter extends Component {
   //   console.log('Clicked!',this)
   // }
 
-  handleIncrement = () => {
-    // console.log("Clicked!", this);
-
-    this.setState({
-      count: this.state.count + 1,
-    });
-  };
-
   render() {
+    console.log("props", this.props);
+
     return (
       // Fragment is used to seperate into one root element
       <React.Fragment>
         {/* <h1>Hello World!</h1> */}
         {/* <code>AASHIQ AHMED</code> */}
-
+        {this.props.children}
         <span style={this.styles} className={this.getBadgeClasses()}>
           {this.formatCount()}
         </span>
         <button
           className="btn btn-secondary btn-sm"
-          onClick={this.handleIncrement}
+          onClick={() => this.props.onIncrement(this.props.counter)}
         >
           Increment
         </button>
-        <ul>
-          {this.state.tags.length === 0 && "No tags"}{" "}
-          {/*True && 'Strig' returns true */}
-        
-          {/* {this.renderTags()} */}
-       
-        </ul>
+        <button
+          className="btn btn-danger btn-sm m-2"
+          onClick={() => this.props.onDelete(this.props.counter.id)}
+        >
+          Delete
+        </button>
       </React.Fragment>
     );
   }
@@ -83,12 +57,12 @@ class Counter extends Component {
   //   This function is created using Refractor(ctrl+shift+R)
   getBadgeClasses() {
     let classes = "badge m-2 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.props.counter.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state; //We are using curly braces,cos count is array
+    const count = this.props.counter.value;
     return count === 0 ? "Zero" : count;
   }
 }
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -5,20 +5,65 @@ class Counters extends Component {
   state = {
     counters: [
       //Array of objects
-      { id: 1, value: 0 },
+      { id: 1, value: 4 },
       { id: 2, value: 0 },
       { id: 3, value: 0 },
       { id: 4, value: 0 },
-      { id: 5, value: 0 },
     ],
   };
 
+  handleIncrement = (counter) => {
+    // console.log(counter);
+    const counters = [...this.state.counters];
+
+    //We can get the position of counter by using indexOf()
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+
+    // Incrementing the value of the specific index's counter
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleDelete = (counterId) => {
+    // console.log("Event Handler called",counterId);
+
+    // Here the 'filter' method is used to filter all the props except the passed id and thus updating the state
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+    console.log("Reset clicked", this.props);
+  };
   render() {
+    //  console.log("props", this.props);
     return (
       <div>
+        <button
+          className="btn btn-primary btn-sm m-2"
+          onClick={this.handleReset}
+        >
+          Reset
+        </button>
         {/* Rendering Components dynamically */}
         {this.state.counters.map((counter) => (
-          <Counter key={counter.id} />
+          <Counter
+            // You can ask why we use id and key as their values are the same,its coz key is internally processed
+            // The key is an attribute that you must pass to all components created dynamically from an array.
+            // It's a unique and constant id('key') that React uses to identify each component in the DOM and to know whether it's a different component or the same one.
+            key={counter.id}
+            onIncrement={this.handleIncrement}
+            onDelete={this.handleDelete}
+            counter={counter} //The 'counter' variable in the map() function now holds the id and value
+          >
+            <h1>Title #{counter.id}</h1>
+          </Counter>
         ))}
       </div>
     );
